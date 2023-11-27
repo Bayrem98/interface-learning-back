6520f243c8b254d8a6219db346a485145bdf86ee
bayrem98 <bayremsayeh98@gmail.com>
2023-11-27 13:36:08 +0100

    change code cloudinarymo & uploadmo to fix error

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
+  imports: [CloudinaryModule],
   controllers: [UploadController],
   providers: [UploadService],
 })
