f3f74239e866befee23f2fea13c372516166dbc6
bayrem98 <bayremsayeh98@gmail.com>
2023-11-28 10:28:23 +0100

    add cloudinaryprovider in upload mo in provider

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { CloudinaryProvider } from 'src/cloudinary/cloudinary.provider';
 
 @Module({
   imports: [CloudinaryModule],
   controllers: [UploadController],
-  providers: [UploadService],
+  providers: [UploadService, CloudinaryProvider],
 })
 export class UploadModule {}
