890bce99379c1e27d80d5df57c573d5ba20e8682
bayrem98 <bayremsayeh98@gmail.com>
2023-11-28 10:48:19 +0100

    laste update to fix error 500 to uploads files

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -4,11 +4,9 @@ export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   useFactory: () => {
     return cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_NAME || 'dbnljryjc',
-      api_key: process.env.CLOUDINARY_API_KEY || '552237126716848',
-      api_secret:
-        process.env.CLOUDINARY_API_SECRET || 't1yOFzk9qiMDLTbO416jzHc5X00',
-      secure: true,
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
   },
 };
